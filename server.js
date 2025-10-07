16a237fdc3c269bbd1bb6330381b2dd3fdc41cb4
snehakushwah23 <sk0069954@gmail.com>
2025-10-07 22:27:23 +0530

    Remove dotenv dependency - use Render env vars

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
